064c04f3b522d62fd0f0f1515d9f3ae46bd79f89
Carlos Chinchilla <calufa@gmail.com>
2020-02-04 16:21:46 -0500

    add config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# Warning: the position of these variables is sensitive.
+# Keep them on top.
+PUNCT_BACK_DICTIONARY = {
+	'xxx0001': '!".'
+}
+# End of sensitive variables.
+AVG_DURATION_PER_WORD = 0.5 # in seconds
+BREAK_TOKENS = ['.', ',', ';', '?', '!'] + list(PUNCT_BACK_DICTIONARY.keys())
+CAPTIONS_FILE_PATH = '/data/captions.vtt'
+CAPTIONS_STYLE = 'align:center line:-9 position:50% size:80%'
+END_DURATION_OFFSET = 1.5 # in seconds
+MAX_CAPTION_LETTERS = 35
+MAX_NUM_CAND_SEARCH_ATTEMPTS = 50000
+MAX_SKIP_TOKENS = 3
+MIN_CAPTION_LETTERS = 25
+NEW_LINE_TOKENS = ['.', '?', '!']
+PUNCT_TO_DICTIONARY = {
+	'!".': ' xxx0001'
+}
+SPACY_MODEL_NAME = 'en_core_web_sm'
+STORY_FILE_PATH = '/data/story.yaml'
