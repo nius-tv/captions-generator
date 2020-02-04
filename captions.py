3c977e8487c136c1a97bbcb2a61a4062baeb839c
Carlos Chinchilla <calufa@gmail.com>
2020-02-04 16:42:55 -0500

    add captions.py

---
+++ captions.py
@@ -0,0 +1,33 @@
+import yaml
+
+from config import *
+from google.cloud import error_reporting
+
+
+def load_story():
+	with open(STORY_FILE_PATH) as f:
+		data = f.read()
+	return yaml.load(data, Loader=yaml.FullLoader)
+
+
+if __name__ == '__main__':
+	error_client = error_reporting.Client()
+	try:
+		# Load story
+		story = load_story()
+		text = story['text']
+		fa_words = story['forcedAligner']['words']
+		# Convert text to timestamp-phrases
+		ts_phrases = text_to_phrase.convert(text, fa_words)
+		# Convert timestamp-phrases to captions
+		captions = phrases_to_captions(ts_phrases)
+		# Convert captions to vtt-captions
+		duration = story['duration']
+		data = captions_to_vtt(captions, duration)
+
+		with open(CAPTIONS_FILE_PATH, 'w') as f:
+			f.write(data)
+
+	except Exception:
+		error_client.report_exception()
+		raise
