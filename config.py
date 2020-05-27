39f452211ddee90acb95f276be80a34dd3d4f10d
Carlos Chinchilla <calufa@gmail.com>
2020-05-26 23:10:08 -0600

    remove dead code

--- config.py
+++ config.py
@@ -1,21 +1,9 @@
-# Warning: the position of these variables is sensitive.
-# Keep them on top.
-PUNCT_BACK_DICTIONARY = {
-	'xxx0001': '!".'
-}
-# End of sensitive variables.
-AVG_DURATION_PER_WORD = 0.5 # in seconds
-BREAK_TOKENS = ['.', ',', ';', '?', '!'] + list(PUNCT_BACK_DICTIONARY.keys())
+BREAK_TOKENS = ['.', ',', ';', '?', '!']
 CAPTIONS_FILE_PATH = '/data/captions.vtt'
 CAPTIONS_STYLE = 'align:center line:-9 position:50% size:85%'
 END_DURATION_OFFSET = 1.5 # in seconds
 MAX_CAPTION_LETTERS = 35
 MAX_NUM_CAND_SEARCH_ATTEMPTS = 500
-MAX_SKIP_TOKENS = 50
 MIN_CAPTION_LETTERS = 25
 NEW_LINE_TOKENS = ['.', '?', '!']
-SPACY_MODEL_NAME = 'en_core_web_sm'
 STORY_FILE_PATH = '/data/story.yaml'
-PUNCT_TO_DICTIONARY = {
-	'!".': ' xxx0001'
-}
